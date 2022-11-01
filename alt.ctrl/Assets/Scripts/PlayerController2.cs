53b4dc10c9757eb9718ea5df75a2a246f53d7a03
dawsonpeloquin <72815199+DepressedGecko@users.noreply.github.com>
2022-11-01 13:39:39 -0400

    Update PlayerController2.cs

--- alt.ctrl/Assets/Scripts/PlayerController2.cs
+++ alt.ctrl/Assets/Scripts/PlayerController2.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController2 : MonoBehaviour
 {
+
+    public float maxMoveSpeed = 10;
+    public float smoothTime = 0.3f;
+    Vector3 currentVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +18,13 @@ public class PlayerController2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Vector3 targetPos;
         float distanceToScreen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         targetPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen));
         transform.position = targetPos;
+
+        targetPos = Vector3.SmoothDamp(transform.position, targetPos, ref currentVelocity, smoothTime, maxMoveSpeed);
+
     }
 }
