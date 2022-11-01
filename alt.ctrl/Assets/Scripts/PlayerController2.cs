bdc0e6dd4f01bf7a9c20340abebff81ead7e31f5
dawsonpeloquin <72815199+DepressedGecko@users.noreply.github.com>
2022-11-01 13:02:43 -0400

    basic mouse following CC

---
+++ alt.ctrl/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos;
+        float distanceToScreen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        targetPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen));
+        transform.position = targetPos;
+    }
+}
