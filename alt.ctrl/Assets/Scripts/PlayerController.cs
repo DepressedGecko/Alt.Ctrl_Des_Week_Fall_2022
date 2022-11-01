d6fcf2d43d055d0d9b1da1222960bfa669c9a2b3
syabdulr <syabdulr@sheridancollege.ca>
2022-11-01 13:38:28 -0400

    Background
    Added background.

--- alt.ctrl/Assets/Scripts/PlayerController.cs
+++ alt.ctrl/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     // Start is called before the first frame update
+
+    private Rigidbody2D rb;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
+
+    Vector3 worldPosition;
     void Update()
     {
-        
+    
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
+
 }
