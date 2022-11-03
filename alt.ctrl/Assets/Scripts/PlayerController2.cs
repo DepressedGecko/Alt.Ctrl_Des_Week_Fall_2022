c2ee5cf9022e3c79c6694eb02b4503b3f8f72559
Axel <63276968+FlickrAV@users.noreply.github.com>
2022-11-03 14:28:35 -0400

    click to recenter

--- alt.ctrl/Assets/Scripts/PlayerController2.cs
+++ alt.ctrl/Assets/Scripts/PlayerController2.cs
@@ -23,26 +23,23 @@ public class PlayerController2 : MonoBehaviour
         Vector3 targetPos;
         float distanceToScreen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         targetPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToScreen));
-        transform.position = targetPos;
+        transform.position = targetPos;*/
         
     
         if(control == true)
         {
             Cursor.lockState = CursorLockMode.None;
-            //Cursor.visible = true;
-
             control = false;
         }
 
         
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetMouseButtonDown(0))
         {
-            Debug.Log("SPACE");
+            Debug.Log("Recenter");
             Cursor.lockState = CursorLockMode.Locked;
-            //Cursor.visible = false;
             control = true;
         }
-        */
+        
 
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
